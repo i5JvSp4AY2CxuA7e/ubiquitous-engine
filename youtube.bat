86c3f62733bcbfd671ca826511979aae09d4fcd9
i5JvSp4AY2CxuA7e <142115943+i5JvSp4AY2CxuA7e@users.noreply.github.com>
2023-09-14 15:14:06 +0200

    Update youtube.bat

--- youtube.bat
+++ youtube.bat
@@ -6,7 +6,7 @@ SET /P URL="[Enter video URL] "
 ECHO.
 ECHO ======================================================================================================================
 ECHO.
-yt-dlp -o YouTube/%%(title)s.%%(ext)s -S "ext:mp4, height:1080, fps:60" --hls-prefer-native --ignore-config --download-archive zzYouTube-Archive.txt %vid% %URL%
+yt-dlp -o YouTube/%%(title)s.%%(ext)s -S "ext:mp4, height:1080, fps:60" --hls-prefer-native --ignore-config --download-archive Archive-YouTube.txt %vid% %URL%
 ECHO.
 ECHO ======================================================================================================================
 ECHO.

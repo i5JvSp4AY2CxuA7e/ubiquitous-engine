698b15e64de6d5e0379775a364bd63440a3c8006
i5JvSp4AY2CxuA7e <142115943+i5JvSp4AY2CxuA7e@users.noreply.github.com>
2023-09-14 15:12:54 +0200

    Update audio.bat

--- audio.bat
+++ audio.bat
@@ -2,4 +2,4 @@ clear
 
 set /p vid= URL:
 
-yt-dlp -o Audio/%%(title)s.%%(ext)s -x --audio-format mp3 --audio-quality 10 -i --ignore-errors -4 --download-archive zzArchive-Audio.txt %VID%
+yt-dlp -o Audio/%%(title)s.%%(ext)s -x --audio-format mp3 --audio-quality 10 -i --ignore-errors -4 --download-archive Archive-Audio.txt %VID%
